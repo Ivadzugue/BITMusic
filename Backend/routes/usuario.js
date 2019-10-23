6a742eb874cbe21fb83e7fae010690c44c7c6635
calixzada <anacbcast@gmail.com>
2019-10-22 20:34:36 -0500

    Backend 1

--- Backend/routes/usuario.js
+++ Backend/routes/usuario.js
@@ -7,7 +7,7 @@ var UsuarioController = require("../controllers/usuario");
 var api = express.Router();
 
 api.post("/usuario", UsuarioController.crearUsuario);
-api.get("/usuario/:id", UsuarioController.obtenerUsuario);
+api.post("/usuario/:id", UsuarioController.obtenerUsuario);
 api.put("/usuario/:id", UsuarioController.actualizarUsuario);
 api.get("/usuario/:id", UsuarioController.eliminarUsuario);
 
